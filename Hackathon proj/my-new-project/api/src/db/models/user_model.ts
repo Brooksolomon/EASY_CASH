910cc2db94633583182e0a8d5f2451547332e6fb
Brooksolomon <86517756+Brooksolomon@users.noreply.github.com>
2023-06-29 09:58:45 +0300

    Your message about the commit

---
+++ Hackathon proj/my-new-project/api/src/db/models/user_model.ts
@@ -0,0 +1,46 @@
+import sequelize from "../sequelize_db";
+import  Sequelize  from "sequelize";
+
+
+const User = sequelize.define('ourusers',  {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement:true,
+    },
+    username: {
+        type: Sequelize.STRING(1234),
+        allowNull: false,
+    },
+    password: {
+        type: Sequelize.STRING(1234),
+        allowNull: false,
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+    balance: {
+        type: Sequelize.DOUBLE,
+        allowNull: true,
+        defaultValue:0
+    },
+    phoneNumber: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+    createdAt: {
+        allowNull: false,
+        defaultValue: Sequelize.fn('now'),
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        defaultValue: Sequelize.fn('now'),
+        type: Sequelize.DATE
+      }
+}  
+);
+ 
+
+export default User;
